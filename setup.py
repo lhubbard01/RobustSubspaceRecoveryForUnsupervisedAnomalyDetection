9f8496cf19015c332aa63a0a75903607ff560186
Lyle Hubbard III <lhubbard@pdx.edu>
2020-09-30 14:20:46 -0700

    Create setup.py
    setup.py created as initial barebones installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+      name="RSRAE",
+      version="0.0.1",
+      author="Lyle Hubbard",
+      author_email="lhubbard@pdx.edu",
+      license="MIT",
+      install_requires=[
+        "torch",
+        ]
+      )
